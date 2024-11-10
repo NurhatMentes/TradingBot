988f4cda4a2d08043b9de93dce3360183e6c6ffe
Nurhat Menteş <nurhatmentes@gmail.com>
2024-11-10 15:56:11 +0300

    feat: Add database migration for Core restructuring - Add migration for User entity move to Core - Update entity relationships - Remove navigation properties - Update base entity structure

---
+++ Infrastructure/Persistence/TradingBotDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class TradingBotDbContextFactory : IDesignTimeDbContextFactory<TradingBotDbContext>
+    {
+        public TradingBotDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<TradingBotDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new TradingBotDbContext(optionsBuilder.Options);
+        }
+    }
+}
